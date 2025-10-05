3946a81851bafc12b63739e9f3887822e88a32f0
Bulk Script <bulk@example.com>
2025-10-05 05:05:01 +0000

    Add random js file 3

---
+++ random_file_3.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3].map(n => n * 2);
+class MyClass { constructor() { this.value = 42; } }
+try { throw new Error('Oops'); } catch (e) { console.error(e); }
+async function fetchData() { await Promise.resolve(); }
+if (condition) { doSomething(); }
+const x = Math.random() * 100;
